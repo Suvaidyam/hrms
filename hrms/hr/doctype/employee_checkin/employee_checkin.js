800254ee35288ed5264c38802796416723bce860
abhiSuvaidyam <abhi.suvaidyam@gmail.com>
2024-03-04 07:26:58 +0000

    Remove auto attendance feature

--- hrms/hr/doctype/employee_checkin/employee_checkin.js
+++ hrms/hr/doctype/employee_checkin/employee_checkin.js
@@ -1,8 +1,6 @@
 // Copyright (c) 2019, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Employee Checkin', {
-	before_save() {
-		console.log('===============HIIIII');
-	},
-});
+// frappe.ui.form.on('Employee Checkin', {
+
+// });
