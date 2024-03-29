c0cde0089845e4ead1d0ec7bb99e15ed6ada6f3f
abhiSuvaidyam <abhi.suvaidyam@gmail.com>
2024-03-29 04:13:20 +0000

    Add

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.55a9hpl117"
+    "revision": "0.3enrgmbrdt8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
